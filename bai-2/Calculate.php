91d4ca51211e095ce54a215f15ecec3738771cdd
tuand4 <vantuantk4@gmail.com>
2019-12-23 14:01:43 +0700

    fix format, validate

--- bai-2/Calculate.php
+++ bai-2/Calculate.php
@@ -1,21 +1,34 @@
 <?php
-class Calculate {
+class Calculate 
+{
     public $num1;
     public $num2;
     public $cal;
+
     public function __construct($num1, $num2, $cal) {
         $this->num1 = $num1;
         $this->num2 = $num2;
         $this->cal = $cal;
     }
-    public function tinh() {
-        if ( $this->num1 == 0 && $this->num2 == 0) {
-            $result = "";
-        } elseif ( $this->num1 == 0 || $this->num2 == 0){
-            $result = "Điền cả 2 số";
+
+    public function tinh() 
+    {
+        $flag = true;
+        if (!is_numeric($this->num1) || !is_numeric($this->num2)) 
+        {
+            $result = "Yều cầu điền số"; 
+            $flag = false;           
         }
-        else {
-            switch ($this->cal) {
+        if (($this->num1 == "") || ($this->num2 == "")) 
+        {
+            $result = "Điền cả 2 số";   
+            $flag = false;       
+        }
+        
+        if ($flag == true)
+        {                   
+            switch ($this->cal) 
+            {
                 case '+':
                     $result = $this->num1 + $this->num2;
                     break;
@@ -26,14 +39,13 @@ class Calculate {
                     $result = $this->num1 * $this->num2;
                     break;
                 case '/':
-                    $result = $this->num1 / $this->num2;
+                    $result = ($this->num2 == 0) ? "không xác định" : $this->num1 / $this->num2;
                     break;
                 default:
                     $result = "Phép tính không hợp lệ";
-                    break;
+                    break;  
             }
         }
         return $result;
     }
 }
- ?>
--- bai-2/index.php
+++ bai-2/index.php
@@ -1,63 +1,54 @@
-<?php
-    include 'Calculate.php';
-    $num1 = $num2 = $cal = $result = "";
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $num1 = $_POST["num1"];
-        $num2 = $_POST["num2"];
-        $cal = $_POST["cals"];    
-        $calculate = new Calculate($num1,$num2,$cal);        
-    }
-   
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-
-    <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-    <!-- Popper JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>    
 </head>
-
 <body>
     <div class="container mt-5">
         <div class="col-6">
+            <?php
+                include 'Calculate.php';
+
+                $result = "";
+                $num1 = isset($_POST["num1"]) ? $_POST["num1"] : "";
+                $num2 = isset($_POST["num2"]) ? $_POST["num2"] : "";
+                $cal = isset($_POST["cals"]) ? $_POST["cals"] : "";
+
+                if (isset($_POST['submit'])) 
+                {
+                    $calculate = new Calculate($num1,$num2,$cal);
+                    $result = $calculate->tinh();
+                }
+            ?>
             <form action="index.php" method="POST">
                 <div class="form-group row">
                     <label class="col-4" for="num1">Số thứ hai</label>
-                    <input  type="number" name="num1" class="form-control col-8" id="num1">
+                    <input  type="text" name="num1" class="form-control col-8" id="num1" value="<?php echo $num1; ?>">
                 </div>
                 <div class="form-group row">
                     <label class="col-4" for="num2">Số thứ hai </label>
-                    <input  type="number" name="num2" class="form-control col-8" id="num2">
+                    <input  type="text" name="num2" class="form-control col-8" id="num2" value="<?php echo $num2; ?>">
                 </div>
                 <div class="form-group row">
                     <label class="col-4" for="cal1">Select list:</label>
                     <select class="form-control col-8" id="cal1" name="cals">
-                        <option value="+">+</option>
-                        <option value="-">-</option>
-                        <option value="*">*</option>
-                        <option value ="/">/</option>
+                        <option value="+" <?php if (isset($_POST['cals']) && $_POST['cals'] == '+') { ?>selected="true" <?php }; ?>>+</option>
+                        <option value="-" <?php if (isset($_POST['cals']) && $_POST['cals'] == '-') { ?>selected="true" <?php }; ?>>-</option>
+                        <option value="*" <?php if (isset($_POST['cals']) && $_POST['cals'] == '*') { ?>selected="true" <?php }; ?>>*</option>
+                        <option value="/" <?php if (isset($_POST['cals']) && $_POST['cals'] == '/') { ?>selected="true" <?php }; ?>>/</option>
                     </select>
                   </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" name="submit" class="btn btn-primary">Submit</button>
             </form>
-            <p class="mt-4"><?php echo $calculate->tinh(); ?></p>
+            <p class="mt-4"><?php echo $result; ?></p>
         </div>
     </div>
 </body>
-
 </html>
